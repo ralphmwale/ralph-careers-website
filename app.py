af22ab4d94c08c020cbf3e7d4511b8efb627cf49
ralphmwale <69141898+ralphmwale@users.noreply.github.com>
2024-01-29 19:09:35 +0000

    added html and css

--- app.py
+++ app.py
@@ -1,8 +1,8 @@
-from flask import Flask
+from flask import Flask, render_template
 app = Flask(__name__) #how a particular script was invoked
 @app.route("/") #part of the url that is accessed after the domain
-def hello_world():
-  return "hello Ralphael"
+def hello_ralph():
+  return render_template('home.html')
 
 if __name__ == "__main__":
   app.run(host='0.0.0.0', debug=True)
